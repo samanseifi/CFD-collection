75ae853fc9857677e653d2b7cb06c13032d2a799
samanseifi <samansei@bu.edu>
2015-10-19 13:57:46 -0400

    added cavity flow

--- Matlab/cavity_flow.m
+++ Matlab/cavity_flow.m
@@ -1,4 +1,4 @@
-function [ output_args ] = cavity_flow( nx, ny, nt, nit, c, rho, nu, dt )
+function [ u,v,p ] = cavity_flow( nx, ny, nt, nit, c, rho, nu, dt )
 
     x = linspace(0,2,nx);
     y = linspace(0,2,ny);
@@ -25,7 +25,22 @@ function [ output_args ] = cavity_flow( nx, ny, nt, nit, c, rho, nu, dt )
             nu*(dt/dx^2*(un(2:end-1, 3:end) - 2*un(2:end-1, 2:end-1) + un(1:end-2, 2:end-1)));
         
         v(2:end-1, 2:end-1) = vn(2:end-1, 2:end-1) - ...
-            
+            un(2:end-1, 2:end-1)*(dt/dx).*(vn(2:end-1, 2:end-1) - vn(2:end-1, 1:end-2)) - ...
+            vn(2:end-1, 2:end-2)*(dt/dy).*(vn(2:end-1, 2:end-1) - vn(1:end-2, 2:end-1)) - ...
+            dt/(2*rho*dy)*(p(3:end, 2:end-1) - p(1:end-2, 2:end-1)) + ...
+            nu*(dt/dx^2*(vn(2:end-1, 3:end) - 2*vn(2:end-1, 2:end-1) + vn(2:end-1, 1:end-2)) + ...
+            (dt/dy^2*(vn(3:end, 2:end-1) - 2*vn(2:end-1, 2:end-1) + vn(1:end-2, 2:end-1)));
+        
+        u(1,:) = 0;
+        u(:,1) = 0;
+        u(:, end-1) = 0;
+        u(end-1, :) = 1;
+                
+        v(1,:) = 0;
+        v(:,1) = 0;
+        v(:, end-1) = 0;
+        v(end-1, :) = 1;
 
+    end
 end
 
